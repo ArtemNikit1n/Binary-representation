a24e728b6728e092b76dcf780fa6d725bc3d83fa
Artem <109523883+ArtemNikit1n@users.noreply.github.com>
2024-10-06 18:56:17 +0300

    Добавлен ввод с клавиатуры

---
+++ BinaryRepresentation/BinaryRepresentation/BinaryRepresentation.c
@@ -0,0 +1,35 @@
+﻿#include <stdbool.h>
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(void) {
+    char strFirstNumber[10];
+    char strSecondNumber[10];
+    char* endptrFirstNumber = NULL;
+    char* endptrSecondNumber = NULL;
+
+    int firstNumber = -1;
+    int secondNumber = -1;
+    bool errorCode = 0;
+
+    setlocale(LC_ALL, "Rus");
+
+    printf("Введите первое число:\n");
+    scanf("%s", strFirstNumber);
+    firstNumber = (int)strtol(strFirstNumber, &endptrFirstNumber, 10);
+
+    printf("Введите второе число:\n");
+    scanf("%s", strSecondNumber); 
+    secondNumber = (int)strtol(strSecondNumber, &endptrSecondNumber, 10);
+
+    if (*endptrFirstNumber != '\0' || *endptrSecondNumber != '\0') {
+        printf("Ошибка ввода");
+        errorCode = 1;
+        return errorCode;
+    }
+
+    return errorCode;
+}
